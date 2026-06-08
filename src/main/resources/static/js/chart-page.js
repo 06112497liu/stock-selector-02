c1d5e67cd936ccb10804dec6d8135703f152acda
reus <reus.lwb@gmail.com>
2026-06-08 22:05:20 +0800

    .4491925232292025:88775c07f25dfe7a4581d8e190ed036f_6a26ca79e08449bbf8216bec.6a26cb5de08449bbf8216bee.6a26cb5d976ded49210d6eae:Trae CN.T(2026/6/8 22:02:05)

--- src/main/resources/static/js/chart-page.js
+++ src/main/resources/static/js/chart-page.js
@@ -16,6 +16,10 @@
   var MA5_COLOR = '#f59e0b';
   var MA10_COLOR = '#3b82f6';
   var MA20_COLOR = '#a855f7';
+  var DIF_COLOR = '#f59e0b';
+  var DEA_COLOR = '#3b82f6';
+  var MACD_UP = '#dc2626';
+  var MACD_DOWN = '#16a34a';
 
   var chart = echarts.init(root);
   var currentPeriod = '1d';
@@ -52,6 +56,82 @@
     return result;
   }
 
+  function calcEMA(closes, period) {
+    var result = [];
+    var k = 2 / (period + 1);
+    var ema = null;
+    for (var i = 0; i < closes.length; i++) {
+      if (i < period - 1) {
+        result.push(null);
+      } else if (i === period - 1) {
+        var sum = 0;
+        for (var j = 0; j < period; j++) {
+          sum += closes[j];
+        }
+        ema = sum / period;
+        result.push(Number(ema.toFixed(4)));
+      } else {
+        ema = closes[i] * k + ema * (1 - k);
+        result.push(Number(ema.toFixed(4)));
+      }
+    }
+    return result;
+  }
+
+  function calcMACD(points) {
+    var closes = [];
+    for (var i = 0; i < points.length; i++) {
+      closes.push(points[i].close);
+    }
+    var ema12 = calcEMA(closes, 12);
+    var ema26 = calcEMA(closes, 26);
+
+    var dif = [];
+    for (var d = 0; d < points.length; d++) {
+      if (ema12[d] !== null && ema26[d] !== null) {
+        dif.push(Number((ema12[d] - ema26[d]).toFixed(4)));
+      } else {
+        dif.push(null);
+      }
+    }
+
+    var dea = [];
+    var difForEma = [];
+    for (var e = 0; e < dif.length; e++) {
+      difForEma.push(dif[e] !== null ? dif[e] : 0);
+    }
+    var deaRaw = calcEMA(difForEma, 9);
+    var firstValidDif = -1;
+    for (var f = 0; f < dif.length; f++) {
+      if (dif[f] !== null) {
+        firstValidDif = f;
+        break;
+      }
+    }
+    for (var g = 0; g < dif.length; g++) {
+      if (firstValidDif >= 0 && g >= firstValidDif + 8) {
+        dea.push(deaRaw[g]);
+      } else {
+        dea.push(null);
+      }
+    }
+
+    var macd = [];
+    for (var m = 0; m < dif.length; m++) {
+      if (dif[m] !== null && dea[m] !== null) {
+        var val = (dif[m] - dea[m]) * 2;
+        macd.push({
+          value: Number(val.toFixed(4)),
+          itemStyle: { color: val >= 0 ? MACD_UP : MACD_DOWN }
+        });
+      } else {
+        macd.push(null);
+      }
+    }
+
+    return { dif: dif, dea: dea, macd: macd };
+  }
+
   // points: [{time, open, high, low, close, volume}, ...]
   function render(points) {
     if (!points || points.length === 0) {
@@ -76,12 +156,13 @@
     var ma5 = calcMA(points, 5);
     var ma10 = calcMA(points, 10);
     var ma20 = calcMA(points, 20);
+    var macdResult = calcMACD(points);
 
     chart.clear();
     chart.setOption({
       animation: false,
       legend: {
-        data: ['K线', 'MA5', 'MA10', 'MA20', '成交量'],
+        data: ['K线', 'MA5', 'MA10', 'MA20', '成交量', 'DIF', 'DEA', 'MACD'],
         top: 0,
         left: 'center'
       },
@@ -91,8 +172,9 @@
       },
       axisPointer: { link: [{ xAxisIndex: 'all' }] },
       grid: [
-        { left: 56, right: 24, top: 48, height: '55%' },
-        { left: 56, right: 24, top: '72%', height: '16%' }
+        { left: 56, right: 24, top: 48, height: '45%' },
+        { left: 56, right: 24, top: '58%', height: '14%' },
+        { left: 56, right: 24, top: '78%', height: '14%' }
       ],
       xAxis: [
         {
@@ -102,6 +184,11 @@
         },
         {
           type: 'category', data: times, gridIndex: 1,
+          boundaryGap: true, axisLine: { onZero: false },
+          axisLabel: { show: false }
+        },
+        {
+          type: 'category', data: times, gridIndex: 2,
           boundaryGap: true, axisLine: { onZero: false }
         }
       ],
@@ -111,11 +198,16 @@
           scale: true, gridIndex: 1, splitNumber: 2,
           axisLabel: { show: false }, axisLine: { show: false },
           axisTick: { show: false }, splitLine: { show: false }
+        },
+        {
+          scale: true, gridIndex: 2, splitNumber: 2,
+          axisLine: { show: true },
+          axisTick: { show: false }, splitLine: { show: false }
         }
       ],
       dataZoom: [
-        { type: 'inside', xAxisIndex: [0, 1], start: 60, end: 100 },
-        { type: 'slider', xAxisIndex: [0, 1], top: '94%', start: 60, end: 100 }
+        { type: 'inside', xAxisIndex: [0, 1, 2], start: 60, end: 100 },
+        { type: 'slider', xAxisIndex: [0, 1, 2], top: '95%', start: 60, end: 100 }
       ],
       series: [
         {
@@ -158,6 +250,29 @@
           name: '成交量', type: 'bar',
           xAxisIndex: 1, yAxisIndex: 1,
           data: volumes
+        },
+        {
+          name: 'DIF', type: 'line',
+          xAxisIndex: 2, yAxisIndex: 2,
+          data: macdResult.dif,
+          smooth: false,
+          symbol: 'none',
+          lineStyle: { color: DIF_COLOR, width: 1 },
+          connectNulls: false
+        },
+        {
+          name: 'DEA', type: 'line',
+          xAxisIndex: 2, yAxisIndex: 2,
+          data: macdResult.dea,
+          smooth: false,
+          symbol: 'none',
+          lineStyle: { color: DEA_COLOR, width: 1 },
+          connectNulls: false
+        },
+        {
+          name: 'MACD', type: 'bar',
+          xAxisIndex: 2, yAxisIndex: 2,
+          data: macdResult.macd
         }
       ]
     });
